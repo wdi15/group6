2aa2c37cc6be47905196e317fc57568b3ac54299
Najzeko <markom@novuscom.net>
2015-08-30 12:42:31 -0700

    fix

--- tim/script.js
+++ tim/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var name = prompt("What is your name so we can log you on?");
 	var welcome = ["Bonjour", "Welcome back", "Hola", "How are you", "Hi"];
-	var num = Math.floor(Math.random() * (welcome.length);
+	var num = Math.floor(Math.random() * (welcome.length));
 	var addition = num==3 ? "? " : " ";
 	$("#change").text(welcome[num] + ", " + name + addition);
 });
